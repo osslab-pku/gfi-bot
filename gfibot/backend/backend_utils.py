f5e148c18e09ea51359730a5d2b263da67a6454b
Haonan Su <kevinsu99@pku.edu.cn>
2022-02-23 12:32:04 +0800

    GFI Backend init

---
+++ gfibot/backend/backend_utils.py
@@ -0,0 +1,10 @@
+from typing import Final
+
+MONGO_URI : Final = 'mongodb://localhost:27017/'
+
+db_issue_dataset : Final = 'issuedataset'
+db_issues : Final = 'issues'
+db_repos : Final = 'repos'
+db_repos_commits : Final = 'repos.commits'
+db_repos_issues : Final = 'repos.issues'
+db_repos_stars : Final = 'repos.stars'
