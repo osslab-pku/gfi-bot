b282fd2a318b54b41e5d7abbc3bd755a087903ea
12f23eddde <12f23eddde@gmail.com>
2023-02-05 23:39:00 +0800

    chore: monorepoify

--- frontend/src/storage/index.ts
+++
@@ -1,8 +0,0 @@
-import { store } from './configureStorage';
-
-export const userInfo = () => ({
-  hasLogin: store.getState().loginReducer.hasLogin,
-  name: store.getState().loginReducer.name,
-  githubLogin: store.getState().loginReducer.loginName,
-  githubToken: store.getState().loginReducer.token,
-});
