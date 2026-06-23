ba9ac7b3d8b2aea829150bba33bc93ffa011fae7
Riya Tyagi <riyatyagi1979@gmail.com>
2026-06-23 22:05:08 +0530

    fix: correct remaining newcomer resolve sorting field

--- gfibot/backend/routes/repos.py
+++ gfibot/backend/routes/repos.py
@@ -170,7 +170,7 @@ def get_paged_repo_brief(
         elif filter == RepoSort.ISSUE_CLOSE_TIME:
             q = q.order_by("issue_close_time")
         elif filter == RepoSort.NEWCOMER_RESOLVE_RATE:
-            q = q.order_by("-r_newcomer_resolve")
+            q = q.order_by("-r_newcomer_resolved")
         elif filter == RepoSort.STARS:
             q = q.order_by("-n_stars")
         else:
